c45ee0fcddcb6cfd8abba4ebea046fcc044190bd
joaomr <joaopedromrodriguez@gmail.com>
2020-09-13 20:19:19 -0300

    step

--- main.py
+++ main.py
@@ -90,7 +90,6 @@ def distribute_tasks(tasks_lists):
         tests_list.append(t)
     return tests_list
 
-
 def show_tables(mpsoc_list):
     """
        Mostra as tabelas de cada mpsoc dos testes.
@@ -100,17 +99,106 @@ def show_tables(mpsoc_list):
         print("Teste " + str(i))
         print(tabulate(mpsoc, tablefmt="fancy_grid"))
 
+def show_tables_step(mpsoc_list):
+    """
+       Mostra as tabelas de cada mpsoc dos testes.
+       :param mpsoc_list: Lista de tabela mpsoc.
+       """
+    for i, mpsoc in enumerate(mpsoc_list):
+        print(tabulate(mpsoc, tablefmt="fancy_grid"))
+
+
+def distribute_tasks_step(tasks_lists, indice):
+    """
+    Distribui tarefas no mpsoc.
+    :param tasks_lists: Lista testes com suas tarefas
+    :returns: lista com as tabelas mpsoc
+    """
+    tests_list = []
+    test = tasks_lists[indice]
+    index = 0
+    table = []
+    # Distribui cargas inserindo uma em cada cluster
+    for i in range(test["mpsoc_y"] * test["mpsoc_x"]):
+        cluster = {"tasks": "", "total_load": 0, "len_tasks": 0}
+        table.append(cluster)
+
+    for i in range(len(test["tasks"])):
+        print(test["tasks"][i]["load"])
+
+    result = (list(map(lambda x: "Tarefas " + x["tasks"] + "\n" + "Carga: " + str(x["total_load"]), table)))
+    t = np.array(result, str).reshape(test["mpsoc_y"], test["mpsoc_x"])
+    tests_list.append(t)
+    show_tables_step(tests_list)
+
+    for i in range(test["mpsoc_y"] * test["mpsoc_x"]):
+        cluster = {"tasks": "", "total_load": 0, "len_tasks": 0}
+        if index < len(test["tasks"]):
+            cluster["tasks"] += "T{}".format(index)
+            cluster["total_load"] += test["tasks"][index]["load"]
+            cluster["len_tasks"] += 1
+            index += 1
+        else:
+            break
+        input('')
+        table[i] = cluster
+        for j in range(index, len(test["tasks"])):
+            print(test["tasks"][j]["load"])
+        result = (list(map(lambda x: "Tarefas " + x["tasks"] + "\n" + "Carga: " + str(x["total_load"]), table)))
+        t = np.array(result, str).reshape(test["mpsoc_y"], test["mpsoc_x"])
+        tests_list[0] = t
+        show_tables_step(tests_list)
+
+    # Insere o restante das tarefas sempre no local com cluster com menor carga
+    while index < len(test["tasks"]):
+        # filtra os cluster disponiveis baseado no numero maximo de tasks_per_pe
+        t_filtered = list(
+            filter(lambda m: m["len_tasks"] < test["mpsoc_y"] * test["mpsoc_x"] * test["tasks_per_pe"], table))
+
+        min_load = min(t_filtered, key=lambda x: x["total_load"])
+        min_load["tasks"] += " T{}".format(index)
+        min_load["total_load"] += test["tasks"][index]["load"]
+        min_load["len_tasks"] += 1
+        index += 1
+        input('')
+        for i in range(index, len(test["tasks"])):
+            print(test["tasks"][i]["load"])
+        result = (list(map(lambda x: "Tarefas " + x["tasks"] + "\n" + "Carga: " + str(x["total_load"]), table)))
+        t = np.array(result, str).reshape(test["mpsoc_y"], test["mpsoc_x"])
+        tests_list[0] = t
+        show_tables_step(tests_list)
+        
+    return tests_list
+
 
 if __name__ == '__main__':
-    # Leitura arquivo de testes
-    tests = read_json_file("tests.json")
-    # Leitura arquivo de apps
-    apps_tests = read_json_file("apps_test.json")
-    # Gera lista de testes contendo cada uma uma lista de tarefas
-    tasks_table = generate_tasks_list(tests, apps_tests)
-    # Ordena as lista de tarefas de cada teste de forma ascendente
-    sorted_tasks_table(tasks_table)
-    # Distribiu as tarefas em uma tabela representando o mpsoc
-    mpsocs = distribute_tasks(tasks_table)
-    # Mostra a tabela de mpsoc
-    show_tables(mpsocs)
+
+    while(True):
+        versao = int(input('Selecione o tipo: Direto - 1 , Passo a Passo - 2\n'))
+        if(versao == 1):
+            # Leitura arquivo de testes
+            tests = read_json_file("tests.json")
+            # Leitura arquivo de apps
+            apps_tests = read_json_file("apps_test.json")
+            # Gera lista de testes contendo cada uma uma lista de tarefas
+            tasks_table = generate_tasks_list(tests, apps_tests)
+            # Ordena as lista de tarefas de cada teste de forma ascendente
+            sorted_tasks_table(tasks_table)
+            # Distribiu as tarefas em uma tabela representando o mpsoc
+            mpsocs = distribute_tasks(tasks_table)
+            # Mostra a tabela de mpsoc
+            show_tables(mpsocs)
+            break
+        elif(versao== 2):
+            indice = int(input('Selecione o teste\n'))
+            # Leitura arquivo de testes
+            tests = read_json_file("tests.json")
+            # Leitura arquivo de apps
+            apps_tests = read_json_file("apps_test.json")
+            # Gera lista de testes contendo cada uma uma lista de tarefas
+            tasks_table = generate_tasks_list(tests, apps_tests)
+            # Ordena as lista de tarefas de cada teste de forma ascendente
+            sorted_tasks_table(tasks_table)
+            # Distribiu as tarefas em uma tabela representando o mpsoc
+            mpsocs = distribute_tasks_step(tasks_table, indice)
+            break
